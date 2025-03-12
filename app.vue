6c8384486eed76e18fcb9390cd79a13d7b9a1b98
adrian <hodurbalder@gmail.com>
2025-03-12 06:35:06 -0600

    instalando dependencias y probando componentes

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h1>Hola mundo</h1>
+
+    <Button variant="outline" class="w-full" >
+        hola mundo
+    </Button>
   </div>
 </template>
+<script setup>
+  import { ref } from 'vue';
+
+  import { Button } from '@/components/ui/button'
+</script>
